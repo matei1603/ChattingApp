c000834b85cad0d248c235f612195f2ca3774bb2
Matei <mateipista57@yahoo.com>
2025-04-27 15:53:24 +0300

    27.04.2025 locatia cu search etc

--- lib/screens/location_picker_screen.dart
+++ lib/screens/location_picker_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.dart';
+import 'package:flutter_google_places_hoc081098/google_maps_webservice_places.dart';
+
+const String kGoogleApiKey = 'AIzaSyB6pRZCHnvSBlqZiawHzuysuNCnVH4sNwM';
+final GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
 
 class LocationPickerScreen extends StatefulWidget {
   final void Function(LatLng) onLocationSelected;
@@ -14,29 +19,61 @@ class _LocationPickerScreenState extends State<LocationPickerScreen> {
   LatLng? selectedLocation;
   GoogleMapController? _mapController;
 
+  void _goToLocation(LatLng latLng) {
+    _mapController?.animateCamera(CameraUpdate.newLatLngZoom(latLng, 15));
+    setState(() {
+      selectedLocation = latLng;
+    });
+  }
+
+  Future<void> _handleSearch() async {
+    final prediction = await PlacesAutocomplete.show(
+      context: context,
+      apiKey: kGoogleApiKey,
+      mode: Mode.overlay,
+      language: "en",
+      components: [Component(Component.country, "ro")],
+    );
+
+    if (prediction != null) {
+      final detail = await _places.getDetailsByPlaceId(prediction.placeId!);
+      final lat = detail.result.geometry!.location.lat;
+      final lng = detail.result.geometry!.location.lng;
+      _goToLocation(LatLng(lat, lng));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Pick a Location")),
+      appBar: AppBar(
+        title: const Text("Pick a Location"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: _handleSearch,
+          )
+        ],
+      ),
       body: Stack(
         children: [
           GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: LatLng(45.9432, 24.9668), // Romania default center
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(45.9432, 24.9668), // Romania default
               zoom: 6,
             ),
+            onMapCreated: (controller) => _mapController = controller,
             onTap: (LatLng latLng) {
               setState(() => selectedLocation = latLng);
             },
             markers: selectedLocation != null
                 ? {
               Marker(
-                markerId: MarkerId('selected'),
+                markerId: const MarkerId('selected'),
                 position: selectedLocation!,
               )
             }
                 : {},
-            onMapCreated: (controller) => _mapController = controller,
           ),
           if (selectedLocation != null)
             Positioned(
@@ -48,7 +85,7 @@ class _LocationPickerScreenState extends State<LocationPickerScreen> {
                   widget.onLocationSelected(selectedLocation!);
                   Navigator.pop(context);
                 },
-                child: Text("Save This Location"),
+                child: const Text("Save This Location"),
               ),
             ),
         ],
